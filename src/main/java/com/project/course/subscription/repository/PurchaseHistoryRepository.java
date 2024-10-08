663b489dfe99096dddb75439c29927d88a4e47bc
supreet <supreet2982002@gmail.com>
2024-10-08 15:42:19 +0530

    updated some correction

--- src/main/java/com/project/course/subscription/repository/PurchaseHistoryRepository.java
+++ src/main/java/com/project/course/subscription/repository/PurchaseHistoryRepository.java
@@ -1,12 +1,10 @@
 package com.project.course.subscription.repository;
 
 import com.project.course.subscription.model.PurchaseHistory;
-import com.project.course.subscription.model.PurchaseSubscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PurchaseHistoryRepository extends JpaRepository<PurchaseHistory,Long> {
 
-    int countByPurchaseSubscription(PurchaseSubscription purchaseSubscription);
 }
